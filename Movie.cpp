c4b49a96d0be30f4119666f1292241c742bb8863
dtucker21 <39545980+dtucker21@users.noreply.github.com>
2018-06-10 20:20:42 -0700

    Add files via upload

--- Movie.cpp
+++ Movie.cpp
@@ -1,6 +1,5 @@
 //I affirm that all code given below was written solely by me, Daniel Tucker, and that any help I received adhered to the rules stated for this exam.
 
-#include "stdafx.h"
 #include "Movie.h"
 #include <iostream>
 #include <string>
